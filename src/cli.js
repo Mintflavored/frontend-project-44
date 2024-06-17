1b5f9e94743a2eb777c43d937d0df3800bacf815
Mintflavored <mint1@duck.com>
2024-06-18 00:22:18 +0300

    update sli.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,14 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
-    var userName = readlineSync.question('May I have your name? ');
+const greetUser = () => {
+    const userName = readlineSync.question('May I have your name? ');
     console.log('Hello, ' + userName + '!');
+    return userName;
 };
+
+const name = () => {
+    const user = greetUser();
+    return user;
+};
+
+export {greetUser, name};
