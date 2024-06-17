d2e30d65d88035541dc353219504459d4458411b
Mintflavored <mint1@duck.com>
2024-06-18 00:59:04 +0300

    update brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
-import greetUser from '../src/cli.js'
+import { greetUser } from '../src/cli.js'
+import even from './brain-even.js'
+
 console.log('Welcome to the Brain Games!')
 greetUser()
 console.log('Answer "yes" if the number is even, otherwise answer "no".')
+even()
