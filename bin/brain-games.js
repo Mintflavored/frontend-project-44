6beb3a437ddfed634dbe67aab7cf91ecbd737059
Mintflavored <mint1@duck.com>
2024-07-07 10:10:29 +0300

    update brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,7 +2,9 @@
 import { askUserName } from '../src/cli.js';
 import even from '../games/brain-even.js';
 import calc from "../games/brain-calc.js";
+import gcd from '../games/brain-gcd.js';
 
 askUserName();
 even();
 calc();
+gcd();
