b5291dc0c3357e27a5c8125965e5524e9e674b91
Mintflavored <mint1@duck.com>
2024-10-07 20:15:26 +0300

    brain-even.js

--- games/brain-even.js
+++ games/brain-even.js
@@ -1,26 +1,27 @@
-import { userName } from '../src/cli.js';
+import { userName, askUserName } from '../src/cli.js';
 import { getRandomNumber } from '../src/random.js';
 import { checkAnswer } from '../src/checkAnswer.js';
-import { askQuestion, answer } from '../src/question.js';
+import { askQuestion } from '../src/question.js';
+
+const isEven = (num) => num % 2 === 0;
 
 const even = () => {
+    askUserName();
     console.log('Answer "yes" if the number is even, otherwise answer "no".');
+    
     for (let i = 0; i < 3; i += 1) {
         const num = getRandomNumber(1, 15);
-
-        askQuestion(num)
-
-        if (answer !== 'yes' && answer !== 'no') {
+        const userAnswer = askQuestion(num);
+        
+        if (userAnswer !== 'yes' && userAnswer !== 'no') {
             console.log('Invalid answer. Please use "yes" or "no".');
             console.log(`Let's try again, ${userName}!`);
             return false;
         }
 
-        const isEven = num % 2 === 0;
-        const correctAnswer = isEven ? 'yes' : 'no';
-
-        if (!checkAnswer(answer, correctAnswer, userName)) {
-            return;
+        const correctAnswer = isEven(num) ? 'yes' : 'no';
+        if (!checkAnswer(userAnswer, correctAnswer, userName)) {
+            return false;
         }
     }
 
