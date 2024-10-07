11d19e033536db7f6d30dc214001ba996e32d56c
Mintflavored <mint1@duck.com>
2024-10-07 20:04:32 +0300

    -m brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,14 +1,4 @@
 #!/usr/bin/env node
 import { askUserName } from '../src/cli.js';
-import even from '../games/brain-even.js';
-import calc from "../games/brain-calc.js";
-import gcd from '../games/brain-gcd.js';
-import progression from '../games/brain-progression.js';
-import prime from "../games/brain-prime.js";
 
 askUserName();
-even();
-calc();
-gcd();
-progression();
-prime();
