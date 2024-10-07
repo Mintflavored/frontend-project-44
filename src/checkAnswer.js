9b608e2265f6910024b1b59fc04175c7c6df7c51
Mintflavored <mint1@duck.com>
2024-10-07 20:59:50 +0300

    update checkAnswer.js

--- src/checkAnswer.js
+++ src/checkAnswer.js
@@ -1,13 +1,15 @@
 const checkAnswer = (answer, correctAnswer, userName) => {
     const strAnswer = String(answer);
     const strCorrectAnswer = String(correctAnswer);
+
     if (strAnswer === strCorrectAnswer) {
-      console.log('Correct!');
-      return true;
+        console.log('Correct!');
+        return true;
+    } else {
+        console.log(`'${strAnswer}' is wrong answer ;(. Correct answer was '${strCorrectAnswer}'.`);
+        console.log(`Let's try again, ${userName}!`);
+        return false;
     }
-    console.log(`'${strAnswer}' is wrong answer ;(. Correct answer was '${strCorrectAnswer}'.`);
-    console.log(`Let's try again, ${userName}!`);
-    return false;
-  };
-  
-  export default checkAnswer;
+};
+
+export { checkAnswer };
