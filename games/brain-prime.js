1094f768b3495e0900d0f5279d8f0ee249969005
Mintflavored <mint1@duck.com>
2024-10-07 19:41:08 +0300

    update brain-prime.js

---
+++ games/brain-prime.js
@@ -0,0 +1,31 @@
+import { checkAnswer } from "../src/checkAnswer.js";
+import { userName } from "../src/cli.js";
+import { askQuestion, answer } from "../src/question.js";
+import { getRandomNumber } from "../src/random.js";
+
+const isPrime = (num) => {
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i += 1) {
+      if (num % i === 0) return false;
+    }
+    return true;
+};
+
+const prime = () => {
+    console.log('Answer "yes" if given number is prime. Otherwise answer "no".')
+  
+    for (let i = 0; i < 3; i += 1) {
+        const number = getRandomNumber(1, 10);
+        askQuestion(number);
+        const correctAnswer = isPrime(number)  ? 'yes' : 'no';
+
+        if (!checkAnswer(answer, correctAnswer, userName)) {
+            return;
+        }
+    }
+
+    console.log(`Congratulations, ${userName}!`);
+    return true;
+}
+
+export default prime;
